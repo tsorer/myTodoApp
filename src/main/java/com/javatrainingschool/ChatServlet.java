32fc69623bbb04fcad74534ff0cd8e137719b0f9
Reto <retostucki87@hotmail.com>
2025-08-05 08:00:57 +0200

    ChatServlet

--- src/main/java/com/javatrainingschool/ChatServlet.java
+++ src/main/java/com/javatrainingschool/ChatServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 @WebServlet("/chat")
 public class ChatServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
